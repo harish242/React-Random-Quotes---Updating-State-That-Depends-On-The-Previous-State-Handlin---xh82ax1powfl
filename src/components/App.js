8ee1406b110feff7d180b2bcb52b7222fc3af951
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-03 08:31:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,49 @@
-import React from "react";
-import {useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
-  var colors = [
-        '#16a085',
-        '#27ae60',
-        '#2c3e50',
-        '#f39c12',
-        '#e74c3c',
-        '#9b59b6',
-        '#FB6964',
-        '#342224',
-        '#472E32',
-        '#BDBB99',
-        '#77B1A9',
-        '#73A857'
-      ];
 
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+];
 
 const App = () => {
-  const[quote,setQuote]=useState({content:"",author:""})
-  const[backgroundColor,setBackgroundColor]=useState('#16a085')
-  useEffect(()=>{
-    fetchquote()
-    },[])
-    const fetchquote=()=>{
-      fetch('https://api.quotable.io/random').then(response=>response.json()).then(data=>{
-        setQuote({content:data.content,author:data.quthor})
-        setBackgroundColor(getRandomColor())
-      }).catch(error=>console.log(error))
-    }
-    const getRandomColor=()=>{
-    
-      const randomIndex=Math.floor(Math.random()*colors.length)
-      return colors[randomIndex]
-    }
-  
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [backgroundColor, setBackgroundColor] = useState("");
 
-    return (
-      <div id="main" style={{backgroundColor}}>
-        <div id="wrapper">
-          <div className='quote-text'>{quote.content}</div>
-          <div className='quote-author'>{quote.author}</div>
-          <button id='new-quote' onClick={fetchquote}>Next quote</button>
-        </div>
-      </div>
-    );
+  const fetchQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.content);
+        setAuthor(data.author);
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+        setBackgroundColor(randomColor);
+      })
+      .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div id="main" style={{ backgroundColor: backgroundColor }}>
+        <div className="quote-text">{quote}</div>
+        <div className="quote-author">{author}</div>
+        <button id="new-quote" class="button" onClick={fetchQuote}>Next Quote</button>
+    </div>
+  );
 };
 
 export default App;
