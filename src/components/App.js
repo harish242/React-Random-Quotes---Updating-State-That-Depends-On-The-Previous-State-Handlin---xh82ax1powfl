7932fdce8d26ba320b75d322a7761a3cf3e2a54d
harish242 <120125264+harish242@users.noreply.github.com>
2023-06-03 08:24:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 import {useState,useEffect} from 'react'
 import "../styles/App.css";
-
+  var colors = [
+        '#16a085',
+        '#27ae60',
+        '#2c3e50',
+        '#f39c12',
+        '#e74c3c',
+        '#9b59b6',
+        '#FB6964',
+        '#342224',
+        '#472E32',
+        '#BDBB99',
+        '#77B1A9',
+        '#73A857'
+      ];
 
 
 const App = () => {
@@ -17,20 +30,7 @@ const App = () => {
       }).catch(error=>console.log(error))
     }
     const getRandomColor=()=>{
-      var colors = [
-        '#16a085',
-        '#27ae60',
-        '#2c3e50',
-        '#f39c12',
-        '#e74c3c',
-        '#9b59b6',
-        '#FB6964',
-        '#342224',
-        '#472E32',
-        '#BDBB99',
-        '#77B1A9',
-        '#73A857'
-      ];
+    
       const randomIndex=Math.floor(Math.random()*colors.length)
       return colors[randomIndex]
     }
